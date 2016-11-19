99ec3b5a1aca17e44ba5d857f0e8d830f171f731
Easy.RaspBerry <sempronx86@gmail.com>
2016-11-19 04:23:07 +0000

    修改数据库连接地址

--- config.php
+++ config.php
@@ -8,7 +8,7 @@
 header("Content-type:text/html; charset=utf-8");
 
 define("DB_NAME","rmcoffee");//数据库名称
-define("DB_HOST","192.168.31.218");//数据库访问地址
+define("DB_HOST","192.168.31.181");//数据库访问地址
 define("DB_USER","easy");//数据库访问用户名
 define("DB_PWD","123456");//数据库访问用户密码
 
