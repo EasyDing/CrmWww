ff16ce3b29649c15c9a1b9f548c3ad050a3291ee
Easy.D <sempronx86@gmail.com>
2016-12-25 21:27:35 +0800

    Add: check_item

---
+++ warehouse/select_inventory.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Easy.D
+ * Date: 2016/12/25
+ * Time: 下午9:21
+ */
+
+require "../config.php";
+
+
+$con = new mysqli(DB_HOST,DB_USER,DB_PWD,DB_NAME);
+if($con ->connect_errno){
+    die('Connect Error: '.$con->connect_errno);
+}
+
+$con->set_charset('utf8');
+//此处插入数据库操作
+
+$sql = /** @lang text */
+    "SELECT * FROM warehouse_item WHERE warehouseitemWarehouseName = ".$_POST["warehouseitemWarehouseName"];
+
+$con_result = @$con -> query($sql);
+
+if($con_result && $con_result -> num_rows > 0){
+
+    while($row = $con_result -> fetch_assoc()){
+        $rows[] = $row;
+    }
+
+    echo "{\"status\":\"Success\",\"message\":\"\",\"data\":";
+    echo json_encode($rows);
+    echo "}";
+
+}else{
+    echo "{\"status\":\"Fail\",\"message\":\"查询失败\"}";
+}
+
+$con->close();
+
