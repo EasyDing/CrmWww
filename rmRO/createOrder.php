061db4c083e189cff888132c3eeb6594356c690a
Easy.D <sempronx86@gmail.com>
2017-01-20 10:54:57 +0800

    rmRO

--- rmRO/createOrder.php
+++ rmRO/createOrder.php
@@ -18,7 +18,7 @@ $con->set_charset('utf8');
 $sql1=/** @lang text */
     "SELECT classId FROM class WHERE country='". $_POST["country"]."' AND farm='".$_POST["farm"]."' AND classes='".$_POST["classes"]."';";
 $sql3=/** @lang text */
-    "SELECT classId FROM class WHERE country='巴拿马' AND farm='哈特曼' AND classes='红酒卡杜拉';";
+    "SELECT classId FROM class WHERE country='巴拿马' AND farm='哈特曼' AND classes='水洗卡杜拉';";
 
 $con_result = @$con -> query($sql3);
 
@@ -30,7 +30,7 @@ if($con_result && $con_result -> num_rows > 0) {
     echo $classId;
 
     $sql2 = /** @lang text */
-        "INSERT INTO `order` (`cId`,`weight`) VALUES (" . $classId . ",'" . $_POST["weight"] . "'')";
+        "INSERT INTO rmRO.`order` (`cId`,`weight`) VALUES (" . $classId . ",'" . $_POST["weight"] . "'')";
 
     $res = @$con->query($sql2);
 
