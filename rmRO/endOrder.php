3aaa08d15b8aa425eacc18264feeb3ce8fb8b0f3
Easy.D <sempronx86@gmail.com>
2017-01-21 12:56:40 +0800

    rmRO

---
+++ rmRO/endOrder.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Easy.D
+ * Date: 2017/1/21
+ * Time: 下午12:55
+ */
+
+require "./config.php";
+$con = new mysqli(DB_HOST,DB_USER,DB_PWD,DB_NAME);
+if($con ->connect_errno){
+    die('Connect Error: '.$con->connect_errno);
+}
+$con->set_charset('utf8');
+
+$sql=/** @lang text */
+    "UPDATE `order` SET flag = 1 WHERE cId = ".$_POST["cId"].";";
+
+
+
+$con->close();
+?>
